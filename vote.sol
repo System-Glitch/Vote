601acf4fe64ec82dd9a34771c8ed9ba53d41118a
ulphidius <louis_laurent@aol.fr>
2020-12-09 10:25:30 +0100

    set alternative vote contract
    change single turn vote system for a alternative vote system

--- vote.sol
+++ vote.sol
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED;
 
-pragma solidity ^0.7.4;
+pragma solidity ^0.7.5;
 pragma experimental ABIEncoderV2;
 
 
 contract vote {
 
     Candidate[] candidates;
-    Candidate[] candidates2;
     address payable public owner;
     mapping (address => uint) indexByAddress;
     mapping (address => uint) indexByAddress2;
@@ -19,7 +18,7 @@ contract vote {
 
     struct Candidate {
         address myAddress;
-        uint voteCount;
+        uint voteScore;
         string myName;
     }
 
@@ -37,11 +36,12 @@ contract vote {
         require(nb_candidate >= 2);
         require(vote_state == 0);
         require(msg.sender == owner);
+        
         vote_state = 1;
     }
 
-    function get_candidates(bool firstRound) public view returns(Candidate[] memory) {
-        return firstRound ? candidates : candidates2;
+    function get_candidates() public view returns(Candidate[] memory) {
+        return candidates;
     }
     
     function add_candidate (string memory myName) public {
@@ -56,40 +56,27 @@ contract vote {
         nb_candidate++;
     }
 
-    function vote_to(address candidate) public {
-        require (vote_state == 1 && votedByAddress[msg.sender] == false || vote_state == 2 && votedByAddress2[msg.sender] == false );
-        if(vote_state == 1) {
-            votedByAddress[msg.sender] = true;
-            candidates[indexByAddress[candidate]].voteCount++;
-        } else {
-            votedByAddress2[msg.sender] = true;
-            candidates2[indexByAddress2[candidate]].voteCount++;
+    function vote_to(address[] memory vote_candidates) public {
+        require (vote_state == 1 && votedByAddress[msg.sender] == false);
+        require (vote_candidates.length == candidates.length);
+        
+        for (uint i = 0; i < vote_candidates.length; i++) {
+            require (isCandidateByAddress[vote_candidates[i]]);
         }
+        
+        votedByAddress[msg.sender] = true;
+        
+        for (uint y = 0; y < vote_candidates.length; y++) {
+            uint current_candidate_index = indexByAddress[vote_candidates[y]];
+            
+            candidates[current_candidate_index].voteScore += candidates.length - y;
+        }
+        
     }
 
-    function end_turn() public {
-        require(vote_state == 1 || vote_state == 2);
+    function end_vote() public {
+        require(vote_state == 1);
         require(msg.sender == owner);
-        uint save_i1 = 0;
-        uint save_i2 = 0;
-        if (vote_state == 1) {
-	        for(uint i = 0 ; i < nb_candidate ; i++) {
-                if (candidates[i].voteCount > candidates[save_i2].voteCount){
-		            if (candidates[i].voteCount > candidates[save_i1].voteCount){
-					    save_i2 = save_i1;
- 					    save_i1 = i;
-				    } else {
-					    save_i2 = i;
- 			        }
-			    }
-   	 	    }
-   	 	    
-   	 	    candidates2.push(Candidate(candidates[save_i1].myAddress, 0, candidates[save_i1].myName));
-   	 	    indexByAddress2[candidates[save_i1].myAddress] = 0;
-
-   	 	    candidates2.push(Candidate(candidates[save_i2].myAddress, 0, candidates[save_i2].myName));
-   	 	    indexByAddress2[candidates[save_i2].myAddress] = 1;
-        }
 
         vote_state++;
     }
